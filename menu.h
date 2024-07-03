da71b4c2254c5193a23277b7b239483cb3cde496
Dawid Ochman <dawid.ochman99@gmail.com>
2024-07-03 20:56:05 +0200

    Add project files.

---
+++ menu.h
@@ -0,0 +1,13 @@
+enum menu_list
+{
+    add_queue_element,        //dodawanie elementu
+    get_queue_element,        //pobieranie elementu
+    find_particular_element,  //wyszukanie elementu
+    print_queue_elements,     //drukowanie kolejki
+    check_sizeof_queue,        //ile elementow
+    delete_queue,             //usuwanie wszystkich elementow
+    exit_program,             //koniec programu
+    bin_input,                //wejscie dla pliku binarnego
+    bin_output                //wyjscie dla pliku binarnego
+};
+void menu_display();
---
+++ projekt_04_queue_array.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+#include <string.h>
+#pragma wrning(disable:4996)
+extern void menu_display();
+extern void get_option();
+
+int main()
+{
+	printf("Program obslugi kolejki. \n");
+	get_option();
+}
